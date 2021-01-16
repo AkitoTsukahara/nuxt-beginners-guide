5a49d996e8c48680661a21885bcdde010e04afe5
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-01-17 00:26:52 +0900

    sample api app

---
+++ my-first-nuxt-app/pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="container">
+    <div>
+      <h3>Nuxt.jsのタグが付けられた投稿の一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4>
+            <span>{{ item.title }}</span>
+            <small>by {{ item.user.id }}</small>
+          </h4>
+          <div>{{ item.body.slice(0, 130) }}......</div>
+          <p><a :href="item.url">{{ item.url }}</a></p>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+
+    return {
+      items
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
