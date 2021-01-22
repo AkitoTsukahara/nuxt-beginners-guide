501a6df2ffac23866bc5f1a8b5294dcc59f6c21b
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-01-22 23:40:19 +0900

    ユーザページの作成

--- nuxt-blog-service/store/index.js
+++ nuxt-blog-service/store/index.js
@@ -1,4 +1,4 @@
-//import moment from '~/plugins/moment'
+import moment from '~/plugins/moment'
 
 export const state = () => ({
   isLoggedIn: false,
@@ -6,29 +6,28 @@ export const state = () => ({
 })
 
 export const getters = {
-  isLoggedIn: (state) => state.isLoggedIn,
-  user: (state) => state.user
-//   user: state => (state.user ? Object.assign({ likes: [] }, state.user) : null)
+  isLoggedIn: state => state.isLoggedIn,
+  user: state => (state.user ? Object.assign({ likes: [] }, state.user) : null)
 }
 
 export const mutations = {
   setUser(state, { user }) {
-    // if (user.id.match(/_|@|\./)) {
-    //   throw new TypeError('invalid username')
-    // }
+    if (user.id.match(/_|@|\./)) {
+      throw new TypeError('invalid username')
+    }
     state.user = user
     state.isLoggedIn = true
   },
-//   updateUser(state, { user }) {
-//     state.user = user
-//   }
+  updateUser(state, { user }) {
+    state.user = user
+  }
 }
 
 export const actions = {
   async login({ commit }, { id }) {
-    // if (id.match(/_|@|\./)) {
-    //   throw new TypeError('invalid username')
-    // }
+    if (id.match(/_|@|\./)) {
+      throw new TypeError('invalid username')
+    }
     const user = await this.$axios.$get(`/users/${id}.json`)
     console.log(user)
     if (!user.id) throw new Error('Invalid user')
@@ -42,18 +41,18 @@ export const actions = {
     if (!user.id) throw new Error('Invalid user')
     commit('setUser', { user })
   },
-//   async addLikeLogToUser({ commit }, { user, post }) {
-//     user.likes.push({
-//       created_at: moment().format(),
-//       user_id: user.id,
-//       post_id: post.id
-//     })
-//     const newUser = await this.$axios.$put(`/users/${user.id}.json`, user)
-//     commit('updateUser', { user: newUser })
-//   },
-//   async removeLikeLogToUser({ commit }, { user, post }) {
-//     user.likes = post.likes.filter(like => like.user_id !== user.id) || []
-//     const newUser = await this.$axios.$put(`/users/${user.id}.json`, user)
-//     commit('updateUser', { user: newUser })
-//   }
+  async addLikeLogToUser({ commit }, { user, post }) {
+    user.likes.push({
+      created_at: moment().format(),
+      user_id: user.id,
+      post_id: post.id
+    })
+    const newUser = await this.$axios.$put(`/users/${user.id}.json`, user)
+    commit('updateUser', { user: newUser })
+  },
+  async removeLikeLogToUser({ commit }, { user, post }) {
+    user.likes = post.likes.filter(like => like.user_id !== user.id) || []
+    const newUser = await this.$axios.$put(`/users/${user.id}.json`, user)
+    commit('updateUser', { user: newUser })
+  }
 }
